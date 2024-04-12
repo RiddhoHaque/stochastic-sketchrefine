1a663099b2798a863900fa99c22f73910474510e
Riddho Ridwanul Haque <33618344+RiddhoHaque@users.noreply.github.com>
2024-04-12 16:20:44 -0400

    Define some query processing states

---
+++ StochasticPackageQuery/Parser/State/PackageAliasEditingState.py
@@ -0,0 +1,8 @@
+from StochasticPackageQuery.Parser.State.State import State
+from StochasticPackageQuery.Query import Query
+
+
+class PackageAliasEditingState(State):
+
+    def process(self, query: Query, char: chr) -> Query:
+        return query.add_character_to_package_alias(char)
